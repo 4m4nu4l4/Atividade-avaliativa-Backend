78424f1da8110d7ce240374f905a872475562b39
4m4nu4l4 <65728939+4m4nu4l4@users.noreply.github.com>
2024-03-12 19:42:22 -0300

    .

---
+++ index.js
@@ -0,0 +1,13 @@
+//console.log("hello world");
+ const express  = require('express');
+
+ const app = express();
+ app.use(express.json());
+
+ app.get('/',(req,res)=>{
+     res.send("hello world");
+ });
+
+ app.listen(3000,()=>{
+     console.log("server is running on port 3000");
+ });
